487a78898dd37d0d4b7a20279766733917f42b81
Jeffrey Szymanski <jeszyman@gmail.com>
2020-06-15 14:15:39 -0500

    /home/jeszyman/repos/basecamp

--- bin/git-push-all.sh
+++ bin/git-push-all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-STAMP=$(date +%Y-%m-%d)
-for d in /home/jeszyman/repos*
-do
-    [[ ! -d "$d" ]] && continue
-    echo "$d" &&
-    cd "$d" &&
-    git pull && add * && git commit -am "$STAMP" && git push
-    cd "$OLDPWD"
+d=$(date +%Y-%m-%d)
+for d in $HOME/repos/*
+do 
+	[[ ! -d "$d" ]] && continue
+	echo "$d"
+	cd "$d"
+	git add -A && git commit -a -m "$d" && git push
 done
