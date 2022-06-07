7a2ebd8d1d42ca044efd30581d946568225b1723
Jeffrey Szymanski <jeszyman@gmail.com>
2022-06-07 13:43:07 -0500

    .

---
+++ tools/shell/git_push_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+d=$(date +%Y-%m-%d)
+for d in $HOME/repos/*
+do
+	[[ ! -d "$d" ]] && continue
+	echo "$d"
+	cd "$d"
+        git pull
+	git add -A && git commit -m. && git push
+done
