38769dc97e2cde3e839282a60e0ecb99456282e1
Jeffrey Szymanski <jeszyman@gmail.com>
2023-05-15 15:57:04 -0500

    .

--- src/bash_functions.sh
+++ src/bash_functions.sh
@@ -1,3 +1,33 @@
+check_local_software(){
+    # Usage
+    ##
+    ## Message
+    usage(){
+                #########1#########2#########3#########4#########5#########6#########7#########8
+        printf "\n check_local_software
+                \n Checks list of software in bash array and exits with error message if any
+ software in the array is not found
+                \n \$1 = Bash array of software, e.g. try software=(bash, git)
+                \n "
+    }
+    ##
+    ## Check
+    [[ $# -eq 0 ]] && usage
+    #
+    # Variables
+    software=$1
+    #
+    # Run
+    for i in "${software[@]}"; do
+    if command -v $i >/dev/null 2>&1 ; then
+        echo "$i installed"
+    else
+        echo "$i not found, exiting"
+        exit 1
+    fi
+done
+}
+
 #
 ######################
 ### symlink-by-csv ###
@@ -33,33 +63,3 @@ symlink_by_csv(){
 # cat path-to-link-to.txt
 # cd ../
 # rm -rf csv-symlink-test
-
-check_local_software(){
-    # Usage
-    ##
-    ## Message
-    usage(){
-                #########1#########2#########3#########4#########5#########6#########7#########8
-        printf "\n check_local_software
-                \n Checks list of software in bash array and exits with error message if any
- software in the array is not found
-                \n \$1 = Bash array of software, e.g. try software=(bash, git)
-                \n "
-    }
-    ##
-    ## Check
-    [[ $# -eq 0 ]] && usage
-    #
-    # Variables
-    software=$1
-    #
-    # Run
-    for i in "${software[@]}"; do
-    if command -v $i >/dev/null 2>&1 ; then
-        echo "$i installed"
-    else
-        echo "$i not found, exiting"
-        exit 1
-    fi
-done
-}
---
+++ tools/shell/conda_from_yaml.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yaml="${1}"
+
+# Name of the Conda environment
+env_name=$(cat $yaml | grep name: | sed 's/^.*\ //g')
+
+# Check if the environment already exists
+if conda env list | grep -q "^$env_name\s"; then
+    # Update the existing environment
+    mamba env update --name "$env_name" --file "$yaml" --prune
+else
+    # Create a new environment
+    mamba env create -n "$env_name" -f "$yaml"
+fi
