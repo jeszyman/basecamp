dc4872736ae0d4e84bef6f9e7ce65e2765c773ee
jeszyman <jeszyman@gmail.com>
2025-04-08 15:49:10 -0500

    .

--- emacs/init.el
+++ emacs/init.el
@@ -49,6 +49,6 @@
 
 ;; Load late configuration
 (safe-load-file-if-exists "~/.emacs.d/load-last.el")
+(setq server-socket-dir (expand-file-name "server" user-emacs-directory))
 (require 'server)
-(setq server-auth-dir (expand-file-name "server" user-emacs-directory))
 (unless (server-running-p) (server-start))
